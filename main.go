40328ba05eb2185d4a2fcc961ec740f9e8265244
Jacob B. Sanders <segmentational@gmail.com>
2024-03-07 23:12:27 -0700

    Clean-Up, Logging, Simplification

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -21,7 +19,6 @@ func init() {
 		unleash.WithUrl("https://gitlab.com/api/v4/feature_flags/unleash/53169971"),
 		unleash.WithInstanceId("9oBgyhUPSdr5GT2Favwb"),
 		unleash.WithAppName("Production"), // Set to the running environment of your application
-		unleash.WithListener(&unleash.DebugListener{}),
 		unleash.WithDisableMetrics(true),
 		unleash.WithRefreshInterval(time.Second*15),
 	)
@@ -33,14 +30,6 @@ func init() {
 	}
 
 	unleash.WaitForReady()
-}
-
-func flags() map[string]interface{} {
-	return map[string]interface{}{
-		"user": map[string]interface{}{
-			"metadata": unleash.IsEnabled("user-metadata"),
-		},
-	}
 }
 
 // server initializes a http.Server with application-specific configuration.
@@ -63,43 +52,22 @@ func server(ctx context.Context, handler http.Handler, port string) *http.Server
 	}
 }
 
-// interrupt is a graceful interrupt + signal handler for an HTTP server.
-func interrupt(ctx context.Context, cancel context.CancelFunc, server *http.Server) {
-	// Listen for syscall signals for process to interrupt/quit
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		<-interrupt
-
-		slog.InfoContext(ctx, "Initializing Server Shutdown ...")
-
-		// Shutdown signal with grace period of 30 seconds
-		shutdown, timeout := context.WithTimeout(ctx, 30*time.Second)
-		defer timeout()
-		go func() {
-			<-shutdown.Done()
-			if errors.Is(shutdown.Err(), context.DeadlineExceeded) {
-				slog.Log(ctx, slog.LevelError, "Graceful Server Shutdown Timeout - Forcing an Exit ...")
-
-				os.Exit(99)
-			}
-		}()
-
-		// Trigger graceful shutdown
-		if e := server.Shutdown(shutdown); e != nil {
-			slog.Log(ctx, slog.LevelError, e.Error())
-		}
-
-		cancel()
-	}()
-}
-
 // router contains the http endpoints used to serve http request(s).
 func router() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		content, e := json.MarshalIndent(flags(), "", "    ")
+		ctx := r.Context()
+
+		slog.DebugContext(ctx, "Serving HTTP(s) Request", slog.String("url", r.URL.String()))
+
+		flags := map[string]interface{}{
+			"user": map[string]interface{}{
+				"metadata": unleash.IsEnabled("user-metadata"),
+			},
+		}
+
+		content, e := json.MarshalIndent(flags, "", "    ")
 		if e != nil {
 			code := http.StatusInternalServerError
 			message := http.StatusText(code)
@@ -116,28 +84,25 @@ func router() http.Handler {
 	return mux
 }
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
+func logger() *slog.Logger {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+}
 
-	api := server(ctx, router(), "3000")
+func main() {
+	ctx := context.Background()
 
-	interrupt(ctx, cancel, api)
+	slog.SetDefault(logger())
 
-	// <-- blocking
+	api := server(ctx, router(), "8080")
+	slog.InfoContext(ctx, "Starting HTTP Server", slog.String("hostname", "localhost"), slog.String("port", "8080"))
 	if e := api.ListenAndServe(); e != nil && !(errors.Is(e, http.ErrServerClosed)) {
 		slog.ErrorContext(ctx, "Error During Server's Listen & Serve Call ...", slog.String("error", e.Error()))
 
 		os.Exit(100)
-	}
-
-	// --> exit
-	{
-		slog.InfoContext(ctx, "Graceful Shutdown Complete")
-
-		// Waiter
-		<-ctx.Done()
 	}
 
 	os.Exit(0)
